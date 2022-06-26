a0dc008f3568776219fc6fc600c71c6cc35c5190
Jenna Tucker <jennatuckerdeveloper@gmail.com>
2022-06-26 16:32:46 -0700

    note about passing children

--- without_jsx/main.js
+++ without_jsx/main.js
@@ -1,8 +1,8 @@
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-// React.createElement takes element string or Component, props, children.  
+// React.createElement takes element string or Component, props, children.
 
-// JSX version:   
+// JSX version:
 
 // class HelloJSX extends React.Component {
 // 	render() {
@@ -10,28 +10,37 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 // 	}
 // }
 
-// root.render(<HelloJSX toWhat='World!' />)  // => Won't run! 
+// root.render(<HelloJSX toWhat='World!' />)  // => Won't run!
 
 // Without JSX version:
 
 class Hello extends React.Component {
 	render() {
-		return React.createElement('div', null, `Hello ${this.props.toWhat}`)
+		return React.createElement('div', null, `Hello ${this.props.toWhat}`, [
+			...this.props.children
+		])
 	}
 }
 
-root.render(React.createElement(Hello, { toWhat: 'World!' }, null)) // Runs!
+root.render(
+	React.createElement(
+		Hello,
+		{ toWhat: 'World!' },
+		React.createElement('div', {}, 'child 1!'), // When you pass more than one, they become an array.
+		React.createElement('div', {}, 'child 2!') // Wrapping these two in an array works the same, but throw key prop error.
+	)
+) // Runs!
 
-// Most basic version.  
+// Most basic version.
 // root.render(
 // 	React.createElement(
 //     'h2',
 //     null,
-//     'Greetings'
+// 		'Greetings'
 // 	)
 // )
 
-// Shows that render can return an array.  This will throw key prop error.  
+// Shows that render can return an array.  This will throw key prop error.
 class HelloArray extends React.Component {
 	render() {
 		return [
@@ -46,7 +55,7 @@ class HelloArray extends React.Component {
 
 const children = ['Hello World!', 'Hello again!']
 
-// With class Component.  
+// With class Component.
 class BasicClass extends React.Component {
 	render() {
 		return React.createElement(
@@ -69,12 +78,10 @@ class BasicClass extends React.Component {
 
 const BasicFunc = ({ children }) => {
 	return React.createElement(
-			'div',
-			null,
-			children.map((text, i) =>
-				React.createElement('p', { key: i }, text)
-			)
-		)
+		'div',
+		null,
+		children.map((text, i) => React.createElement('p', { key: i }, text))
+	)
 }
 
 // const element = React.createElement(
